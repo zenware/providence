826628ce60dedbdd35c3ec80aeb8b25c6ae446f3
Jay Looney <jay.m.looney@gmail.com>
2017-02-14 05:02:58 -0600

    Setup the Initial Layout of the Frontend Project
    This included adding the bootstrap to the project packages and adding
    it to the imports, as well as defining a few React Elements and setting
    some hard-coded CSS.
    
    Next steps are to actually build in a backend endpoint that can spit
    out data appropriate for this app. That's going to involve extracting
    functionality from my `dnsanalyzergo` project into a library. I also
    plan to mess around with GAE for deployment, but may go a different
    route due to database fees.

--- src/App.js
+++ src/App.js
@@ -2,17 +2,95 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class TableField extends Component {
+  handleClick() {
+    // TODO(zenware): Figure out how to get this clipboard nonsense working...
+    const copystring = this.props.fieldName + " = " + this.props.data
+    let cpe = document.getElementById("copyspace");
+    cpe.value = copystring
+    cpe.select()
+    document.execCommand('copy')
+    console.log(copystring)
+  }
+  render() {
+    return (
+      <td className="table-field" onClick={() => this.handleClick()}>
+        {this.props.data}
+      </td>
+    )
+  }
+}
+
+class TableRow extends Component {
+  handleClick(e) {
+    e.preventDefault()
+    // TODO(zenware): this should copy as a dictionary...
+    let copystring = "record = " + JSON.stringify(this.props.data)
+    console.log(copystring)
+  }
+  render() {
+    const fields = Object.getOwnPropertyNames(this.props.data).map((x, idx) => {
+      return <TableField key={idx} fieldName={x} data={this.props.data[x]} />
+    })
+    return (
+      <tr className="table-row" onClick={(e) => this.handleClick(e)}>
+        {fields}
+      </tr>
+    )
+  }
+}
+
+class Table extends Component {
+  constructor() {
+    super()
+    this.state = {
+      rows: [
+        {
+          ip: "4.2.2.2",
+          min: "27.739852ms",
+          avg: "41.894612ms",
+          max: "56.049373ms",
+          std: "10.016575ms",
+        }, 
+        {
+          ip: "8.8.8.8",
+          min: "0s",
+          avg: "49.947716ms",
+          max: "99.895432ms",
+          std: "35.318488ms",
+        }, 
+      ],
+    }
+  }
+  render() {
+    const rows = this.state.rows.map(row => {
+      return <TableRow key={row.ip} data={row} />
+    })
+    // TODO(zenware): Capitalize table header field names.
+    const fields = Object.getOwnPropertyNames(this.state.rows[0]).map(x => {
+      return <th key={x}>{x}</th>
+    })
+    return (
+      <table className="table table-bordered table-hover table-condensed">
+        <thead><tr>{fields}</tr></thead>
+        <tbody>{rows}</tbody>
+      </table>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <textarea id="copyspace"></textarea>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Public DNS Server Response Times</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="container-fluid">
+          <Table />
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.css';
+// TODO(zenware): At least pick some good colors.
+//import 'bootstrap/dist/css/bootstrap-theme.css';
 import './index.css';
 
 ReactDOM.render(
